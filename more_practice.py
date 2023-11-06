b17486e7d8d6fbfc25f0b7cd768bcc7f0649c6e1
David Phillips <149737232+DPhillipsLearn@users.noreply.github.com>
2023-11-06 14:42:50 -0800

    Add files via upload

---
+++ more_practice.py
@@ -0,0 +1,14 @@
+
+
+
+age = int(input("What is your age?"))
+name = input("What is your name?")
+dob = input("what month where you born?")
+def user_id ():
+    print(f"Hello {name}, Welcome to our game.\nI see you where born in {dob} which is awsome.\nThat will make you {age} as of today!!")
+
+
+
+
+
+user_id()
